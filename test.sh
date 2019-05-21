ec555cbf40b9648fc760919f826010e1c4407a47
Tzuhsien Kao <jasonkao0810@gmail.com>
2019-05-21 11:26:39 +0800

    Compile 1 int and return

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+try() {
+  expected="$1"
+  input="$2"
+
+  ./cc "$input" > tmp.S
+  gcc -o tmp tmp.S
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
