26471cdf93327957ef9d3117248de9b54632ad0b
Tzuhsien Kao <jasonkao0810@gmail.com>
2019-06-06 12:31:59 +0800

    modify test.sh

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ try() {
   if [ "$actual" = "$expected" ]; then
     echo "$input => $actual"
   else
-    echo "$expected expected, but got $actual"
+    echo "$input => $expected expected, but got $actual"
     exit 1
   fi
 }
@@ -41,6 +41,7 @@ tryfunc 3 "int main() {return foo(1, 2);}"
 tryfunc 3 "int main() {int a = 1; int b = 2; return foo(a, b);}"
 try 3 "int main() {return foo();} int foo() {return 3;}"
 try 3 "int main() {return foo(1, 2);} int foo(int a, int b) {return a + b;}"
+try 10 "int main() {int i; int a = 0; for (i = 0; i < 10; i = i + 1) a = a + 1; return a;}"
 try 8 "int main() {return fib(6);} int fib(int a) {if (a == 0) return 0; if (a == 1) return 1; return fib(a-1) + fib(a-2);}"
 try 10 "int main() {int a = 0; while (a < 10) a = a + 1; return a;}"
 try 6 "int main() {int a; int b; a = b = 3; return a + b;}"
